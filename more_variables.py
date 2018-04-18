5893e2b0a3e0865d6e3bdcdedd4adb949aba244e
afrozex <nawedafroze281@gmail.com>
2018-04-18 12:41:01 +0530

    more variable & printing

---
+++ more_variables.py
@@ -0,0 +1,14 @@
+my_name='Nawed Afroze'
+my_age=16 #not a lie
+my_height=175 #cm
+my_weight=51 #kg
+my_eyes='blue'
+my_teeth='white'
+my_hair='black'
+
+print"let's talk about %s."%my_name
+print"he's %d cm tall."%my_height
+print "he's weight is %d kg."%my_weight
+print"his weight is not too heavy its normal."
+print"he's got %s eyes & %s hair."%(my_eyes,my_hair) 
+print"his teeth depends on the junk food what they colour this time %s colour teeth."%my_teeth
